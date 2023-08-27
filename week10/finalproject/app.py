45f239f28065cbe3e39e927b4b575a8b680aeb14
riccardoscloud <riccardo.buttini@icloud.com>
2023-08-27 04:18:00 +0100

    Week10

---
+++ week10/finalproject/app.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+
+    return render_template("index.html")
