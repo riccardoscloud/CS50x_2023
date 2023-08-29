6a18fd25fa472955f48e9974285afcb5e5522407
riccardoscloud <riccardo.buttini@icloud.com>
2023-08-29 14:56:44 +0100

    Reinitialized to DnD theme

--- week10/finalproject/app.py
+++ week10/finalproject/app.py
@@ -16,7 +16,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///project.db")
 
 
 @app.after_request
